80d1cd7994f8c317912e3952684a67888a9a9553
ydlysak <lysak.yaroslav00@yandex.ru>
2026-05-27 21:23:29 +0300

    updated inspections service

--- internal/entity/manifest.go
+++ internal/entity/manifest.go
@@ -13,3 +13,23 @@ type Manifest struct {
 	Language types.Language
 	Type     types.ManifestType
 }
+
+func NewManifest() *Manifest {
+	return &Manifest{
+		UUID: uuid.Must(uuid.NewV7()),
+	}
+}
+
+func (m *Manifest) WithFile(filename string, checksum uint64) *Manifest {
+	m.Filename = filename
+	m.Checksum = checksum
+
+	return m
+}
+
+func (m *Manifest) WithLanguageType(lang types.Language, tp types.ManifestType) *Manifest {
+	m.Language = lang
+	m.Type = tp
+
+	return m
+}
